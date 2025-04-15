b1ee8808dfbc6c35fbd9aaf9c549b5e57a123415
Spycka <vpkvietkauskas@gmail.com>
2025-04-15 09:22:41 +0300

    Delete detectors directory

--- detectors/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-License plate detector module for ANPR system.
-Contains different detector implementations for license plate detection.
-"""
-from detectors.yolo11_gpu import YOLO11sGPU
-from detectors.yolo11_rknn import YOLO11sRKNN
-
-__all__ = ['YOLO11sGPU', 'YOLO11sRKNN']
